a1f57e9f93ae01bd4c4799f31dc05c688043734b
ciszakdamian <ciszakdamian@gmail.com>
2020-06-27 00:49:48 +0200

    change finance frontend

--- resources/views/finance.blade.php
+++ resources/views/finance.blade.php
@@ -9,7 +9,7 @@
 @section('content')
 
     <div class="card-header">
-        <h3 class="card-title">Account {{ $account_number }} Detail:</h3>
+        <h3 class="card-title">Account {{ $account_number }} detail:</h3>
     </div>
     <div class="row">
         <div class="col-12 col-sm-6">
@@ -24,51 +24,51 @@
             <div class="info-box bg-yellow">
                 <div class="info-box-content">
                     <span class="info-box-text text-center text-white">Amount spent in last month</span>
-                    <span class="info-box-number text-center text-white mb-0">2000</span>
+                    <span class="info-box-number text-center text-white mb-0">7641,42</span>
                 </div>
             </div>
         </div>
     </div>
 
     <div class="card-header">
-        <h3 class="card-title">Card Detail:</h3>
+        <h3 class="card-title">Credit card detail:</h3>
     </div>
     <div class="row">
         <div class="col-12 col-sm-6">
-            <div class="info-box bg-green">
+            <div class="info-box bg-cyan">
                 <div class="info-box-content">
-                    <span class="info-box-text text-center text-white">Balance</span>
-                    <span class="info-box-number text-center text-white mb-0">2300</span>
+                    <span class="info-box-text text-center text-white">Spend</span>
+                    <span class="info-box-number text-center text-white mb-0">2301,24</span>
                 </div>
             </div>
         </div>
         <div class="col-12 col-sm-6">
             <div class="info-box bg-yellow">
                 <div class="info-box-content">
-                    <span class="info-box-text text-center text-white">Amount spent in last month</span>
-                    <span class="info-box-number text-center text-white mb-0">2000</span>
+                    <span class="info-box-text text-center text-white">Credit limit</span>
+                    <span class="info-box-number text-center text-white mb-0">5000,00</span>
                 </div>
             </div>
         </div>
     </div>
 
     <div class="card-header">
-        <h3 class="card-title">Credit Detail:</h3>
+        <h3 class="card-title">Credit detail:</h3>
     </div>
     <div class="row">
         <div class="col-12 col-sm-6">
-            <div class="info-box bg-green">
+            <div class="info-box bg-cyan">
                 <div class="info-box-content">
-                    <span class="info-box-text text-center text-white">Balance</span>
-                    <span class="info-box-number text-center text-white mb-0">2300</span>
+                    <span class="info-box-text text-center text-white">Total paid off</span>
+                    <span class="info-box-number text-center text-white mb-0">2500,00</span>
                 </div>
             </div>
         </div>
         <div class="col-12 col-sm-6">
             <div class="info-box bg-yellow">
                 <div class="info-box-content">
-                    <span class="info-box-text text-center text-white">Amount spent in last month</span>
-                    <span class="info-box-number text-center text-white mb-0">2000</span>
+                    <span class="info-box-text text-center text-white">Amount of credit</span>
+                    <span class="info-box-number text-center text-white mb-0">10000,00</span>
                 </div>
             </div>
         </div>
